98f3ca257ed57ebdef6b6c3555c854f0b2df9608
Федор Андреев <fedorr47@gmail.com>
2019-07-01 15:48:21 +0300

    add counting sort (#5)

--- Algorithms/Main.cpp
+++ Algorithms/Main.cpp
@@ -15,15 +15,12 @@
 #include "DivideConquer/Quick_sort.h"
 #include "DivideConquer/Inverse_recursive.h"
 #include "DivideConquer/HeapSort.h"
+#include "DivideConquer/CountingSort.h"
 
 using namespace std;
 
 int main() {
-	int arr[] = { 12, 11, 13, 5, 6, 7 };
-	int n = sizeof(arr) / sizeof(arr[0]);
-	heapSort(arr, n);
-	cout << "Sorted array is \n";
-	printArray(arr, n);
+	example_countSort();
 }
 
 
